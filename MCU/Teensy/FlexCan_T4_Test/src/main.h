d0920da2358ee92423ba601888f1c9083a58a5c3
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-09-20 22:32:19 -0500

    integration of auto msgs

--- MCU/Teensy/FlexCan_T4_Test/src/main.h
+++ MCU/Teensy/FlexCan_T4_Test/src/main.h
@@ -1,25 +1,27 @@
-#define USB_SERIAL // Get rid of IDE errors, they are annoying
-#define __MK66FX1M0__
-
 #include "FlexCAN_T4.h"
 #include "WProgram.h"
 #include "messages.def"
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
+
+#define NUM_TX_MB 2
+#define NUM_RX_MB() PP_NARG_MO(CAN_MESSAGES)
+#define TEENSY_CAN CAN1
 
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-// IMPROVE: Use XMacro to get num of MBs
-#define NUM_RX_MAILBOXES 6
-#define NUM_TX_MAILBOXES 6
-
-FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
+FlexCAN_T4<TEENSY_CAN, RX_SIZE_256, TX_SIZE_16> F_Can;
 static CAN_message_t test_msg;
 
+void setupMB(FLEXCAN_MAILBOX MB, uint32_t address, _MB_ptr handler) {
+    F_Can.setMBFilter(MB, address, address);
+    F_Can.enhanceFilter(MB);
+    F_Can.onReceive(MB, handler);
+}
+
 void setTestMessage() {
-    test_msg.id = random(0, 4);
+    test_msg.id = random(0, 2);
     test_msg.buf[0] = 'o';
     test_msg.buf[1] = 'h';
     test_msg.buf[2] = ' ';
@@ -30,39 +32,39 @@ void setTestMessage() {
     // test_msg.buf[7] = '';
 }
 
-void canSniff(const CAN_message_t &msg) {
-    Serial.print("MB ");
-    Serial.print(msg.mb);
-    Serial.print("  OVERRUN: ");
-    Serial.print(msg.flags.overrun);
-    Serial.print("  LEN: ");
-    Serial.print(msg.len);
-    Serial.print(" EXT: ");
-    Serial.print(msg.flags.extended);
-    Serial.print(" TS: ");
-    Serial.print(msg.timestamp);
-    Serial.print(" ID: ");
-    Serial.print(msg.id, HEX);
-    Serial.print(" Buffer: ");
-    for (uint8_t i = 0; i < msg.len; i++) {
-        Serial.print(msg.buf[i], HEX);
-        Serial.print(" ");
-    }
-    Serial.println();
+void sendTestMessage() {
+    setTestMessage();
+    F_Can.write(test_msg);
 }
 
-template <int T>
-void readCan(const CAN_message_t &msg) {
-    Serial.print("MB: ");
-    Serial.print(T);
-    Serial.print(" ");
-    Serial.print("ID: ");
-    Serial.print(msg.id);
-    Serial.print(" ");
-    for (uint8_t i = 0; i < msg.len; i++) {
-        Serial.print((char)msg.buf[i]);
+void setMailboxes() { // Set mailbox filters & handles from def file
+    F_Can.setMBFilter(REJECT_ALL);
+    F_Can.enableMBInterrupts();
+
+#define X(...) ,
+    int MB = NUM_RX_MB();
+#undef X
+
+    // Auto setup requested MBs
+#define X(address, func)                         \
+    setupMB((FLEXCAN_MAILBOX)MB, address, func); \
+    MB--;
+
+    CAN_MESSAGES
+#undef X
+
+    // Auto set number of TX &RX MBs
+#define X(...) ,
+    F_Can.setMaxMB(NUM_TX_MB + NUM_RX_MB());
+    for (uint8_t i = 0; i < NUM_RX_MB(); i++) {
+        F_Can.setMB((FLEXCAN_MAILBOX)i, RX, NONE);
     }
-    Serial.println();
+    for (uint8_t i = NUM_RX_MB(); i < (NUM_TX_MB + NUM_RX_MB()); i++) {
+        F_Can.setMB((FLEXCAN_MAILBOX)i, TX, NONE);
+    }
+#undef X
+
+    F_Can.mailboxStatus();
 }
 
 #endif // __MAIN_H__
