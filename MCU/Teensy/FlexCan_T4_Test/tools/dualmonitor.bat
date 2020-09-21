422f5347e81925651486f3ee55ce7e1aeca087eb
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-09-21 18:14:02 -0500

    retry

--- MCU/Teensy/FlexCan_T4_Test/tools/dualmonitor.bat
+++ MCU/Teensy/FlexCan_T4_Test/tools/dualmonitor.bat
@@ -1,2 +1,2 @@
-start cmd /c "ComMonitor.exe %2 %1 --priority"
-start cmd /c "ComMonitor.exe %3 %1 --priority"
+start cmd /c "ComMonitor.exe %2 %1 --priority -r"
+start cmd /c "ComMonitor.exe %3 %1 --priority -r"
