d218e3e1be6fc15a1076c50f27803c87c059ab6c
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-10-06 21:52:47 -0500

    re add test Msg

--- Teensy36/FlexCan_T4_Test/src/Main.cpp
+++ Teensy36/FlexCan_T4_Test/src/Main.cpp
@@ -13,6 +13,33 @@ void setup(void) {
     setMailboxes();
 }
 
+void test() {
+    struct test_msg {
+        uint32_t id = 0x0102;
+        uint8_t buf[8] = {
+            0xDE,
+            0xAD, // Convert to Short = ‭57005‬
+            0,
+            0xDE, // Convert to long = ‭3735928559‬
+            0xAD,
+            0xBE,
+            0xEF,
+            0,
+        };
+    } test_msg;
+    Serial.begin(115200);
+    delay(5000);
+    static uint32_t timeout = millis();
+    Serial.println("Start!");
+    while (1) {
+        if (millis() - timeout > 3000) {
+            Serial.write((const uint8_t *)&test_msg.id, 2);  // Java: DataStream.readInt
+            Serial.write((const uint8_t *)&test_msg.buf, 8); // Java: DataStream.readByte x 8
+            timeout = millis();
+        }
+    }
+}
+
 void loop() {
     F_Can.events();
     static uint32_t timeout = millis();
@@ -23,8 +50,9 @@ void loop() {
 }
 
 int main(void) {
-    setup();
-    while (1) {
-        loop();
-    }
+    // setup();
+    // while (1) {
+    //     loop();
+    // }
+    test();
 }
