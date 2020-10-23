056b8d7c133da8403f6ccce9ba190eb7d1f97628
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-10-23 02:00:22 -0500

    reorganize

--- Teensy36/MCU_Testing/src/MCU.cpp
+++
@@ -1,1 +0,0 @@
-#include "MCU.h"
--- Teensy36/MCU_Testing/src/MCU.h
+++ Teensy36/MCU_Testing/src/MCU.h
@@ -11,49 +11,6 @@
 #include "Log.h"
 #include "Pins.h"
 #include "State.h"
-
-static struct Starting_t : State::State_extend<Starting_t> {
-private:
-    elapsedMillis timeElapsed;
-    bool firstSetup = true;
-    int count = 5;
-
-public:
-    bool SetupOnce = false;
-    bool enableSetup = true;
-    const char * ID = "Starting";
-    // State_t *nextState = this;
-    // State_t *errorState = this;
-
-    State::ExitCode setup(void) {
-        if (firstSetup) {
-            pinMode(6, OUTPUT);
-            digitalWrite(6, LOW); /* optional tranceiver enable pin */
-
-            Pins::initialize();
-            firstSetup = false;
-            Log.i(ID, "Finished inital Setup");
-        }
-        count = 5;
-        return State::NOERR;
-    };
-
-    State::ExitCode loop(void) {
-
-        if (timeElapsed >= 1000) {
-            timeElapsed = timeElapsed - 1000;
-            Log(ID, "%u", random(300));
-            count--;
-            // sendTestMessage(F_Can);
-            // Serial.println(Pins::getPinValue(A7));
-            // Pins::setPinValue(A6, random(1024));
-        }
-        Pins::update();
-        if (count == 0)
-            return State::DONE;
-
-        return State::NOERR;
-    };
-} Starting;
+#include "MCUStates.h"
 
 #endif // __MCU_H__
