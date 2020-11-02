5a5111dab51c32160a76b0197d6a07a1e134bbca
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-11-02 13:37:07 -0600

    clean it

--- Teensy36/MCU_Testing/src/Main.cpp
+++ Teensy36/MCU_Testing/src/Main.cpp
@@ -1,10 +1,9 @@
-#include "MCU.h"
 #include "Config.def"
+#include "MCU.h"
 
 int main(void) {
     Serial.begin(CONF_TEENSY_BAUD_RATE);
     delay(CONF_TEENSY_INITAL_DELAY);
-    Log("What", "Begiiin");
     State::begin(Starting);
     return 0;
 }
