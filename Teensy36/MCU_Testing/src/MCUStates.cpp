e00ed70fdc3eb3e094e80fd7eb06658f26116fcd
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-11-03 00:04:18 -0600

    stress testing

--- Teensy36/MCU_Testing/src/MCUStates.cpp
+++ Teensy36/MCU_Testing/src/MCUStates.cpp
@@ -15,15 +15,14 @@ State::ExitCode Starting_t::setup(void) {
 
 State::ExitCode Starting_t::loop(void) {
 
-    if (timeElapsed >= 5000) {
-        timeElapsed = timeElapsed - 5000;
+    // if (timeElapsed >= 100) {
+        // timeElapsed = timeElapsed - 100;
         count--;
         // sendTestMessage(F_Can);
         Log(ID, "Oh why hello there");
-        delay(10);
         Log(ID, "A7 Pin Value: ", Pins::getPinValue(A7));
         Pins::setPinValue(A6, random(1024));
-    }
+    // }
     Pins::update();
     if (count == 0)
         return State::DONE;
