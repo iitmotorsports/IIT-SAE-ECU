1d45f302528c80bfc87ae81697532a69c70f307b
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-09 23:50:39 -0600

    update coms

--- comBoth.bat
+++ comBoth.bat
@@ -1,8 +1,8 @@
 @echo off
-set BACK=COM4
-set FRONT=COM5
+set BACK=COM3
+set FRONT=COM4
 set BAUD=115200
-set MODE=Ascii
+set MODE=Hex
 
 set CALL0=..\TeensyToolchain\tools\ComMonitor.exe %BACK% %BAUD% -t%MODE% -r -w -m8 --jsonPath log_lookup.json
 set CALL1=..\TeensyToolchain\tools\ComMonitor.exe %FRONT% %BAUD% -t%MODE% -r -w -m8 --jsonPath log_lookup.json
