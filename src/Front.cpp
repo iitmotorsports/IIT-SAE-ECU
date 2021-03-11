d910c007b0139b7b22790f7c3ab7c13f53df9fa7
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-10 20:24:14 -0600

    send MC0/1 Voltages

--- src/Front.cpp
+++ src/Front.cpp
@@ -34,18 +34,28 @@ static uint32_t BMSSOC() {
     Canbus::clearSemaphore();
 }
 
-static uint32_t powerValue() { // IMPROVE: get power value using three phase values, or find a power value address
+static uint16_t MC0Voltage() {
     Canbus::setSemaphore(ADD_MC0_VOLT);
     int16_t MC0_VOLT = *((int16_t *)(MC0_VOLT_Buffer)) / 10; // Bytes 0-1: DC BUS MC Voltage
+    Canbus::clearSemaphore();
+    return MC0_VOLT;
+}
+
+static uint16_t MC1Voltage() {
+    Canbus::setSemaphore(ADD_MC0_VOLT);
+    int16_t MC1_VOLT = *((int16_t *)(MC1_VOLT_Buffer)) / 10; // Bytes 0-1: DC BUS MC Voltage
+    Canbus::clearSemaphore();
+    return MC1_VOLT;
+}
+
+static uint32_t powerValue() { // IMPROVE: get power value using three phase values, or find a power value address
     Canbus::setSemaphore(ADD_MC0_CURR);
     int16_t MC0_CURR = *((int16_t *)(MC0_CURR_Buffer + 6)) / 10; // Bytes 6-7: DC BUS MC Current
-    int MC0_PWR = MC0_VOLT * MC0_CURR;
-    Canbus::setSemaphore(ADD_MC1_VOLT);
-    int16_t MC1_VOLT = *((int16_t *)(MC1_VOLT_Buffer)) / 10; // Bytes 0-1: DC BUS MC Voltage
     Canbus::setSemaphore(ADD_MC1_CURR);
     int16_t MC1_CURR = *((int16_t *)(MC1_CURR_Buffer + 6)) / 10; // Bytes 6-7:
-    int MC1_PWR = MC1_VOLT * MC1_CURR;
     Canbus::clearSemaphore();
+    int MC0_PWR = MC0Voltage() * MC0_CURR;
+    int MC1_PWR = MC1Voltage() * MC1_CURR;
     return (MC0_PWR + MC1_PWR) / 1000; // Sending kilowatts
 }
 
@@ -108,13 +118,17 @@ void Front::run() {
     getBuffers();
     loadStateMap();
 
+    static int testValue = 0;
+
     while (true) {
         if (timeElapsed >= 20) { // High priority updates
             timeElapsed = 0;
 
+            testValue = Pins::getPinValue(PINS_FRONT_PEDAL1);
+
             readSerial();
 
-            Log.i(ID, "Current Motor Speed:", motorSpeed() + Pins::getPinValue(PINS_FRONT_PEDAL1)); // TODO: remove test pedal value
+            Log.i(ID, "Current Motor Speed:", motorSpeed() + testValue); // TODO: remove test pedal value
         }
         if (timeElapsedMid >= 200) { // Med priority updates
             timeElapsedMid = 0;
@@ -127,10 +141,10 @@ void Front::run() {
             Pins::setPinValue(PINS_FRONT_BMS_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_BMS_FAULT));
             Pins::setPinValue(PINS_FRONT_IMD_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_IMD_FAULT));
 
-            // TODO: Send both mc voltages
-
-            Log.i(ID, "Current Power Value:", powerValue() + Pins::getPinValue(PINS_FRONT_PEDAL1));   // Canbus message from MCs
-            Log.i(ID, "BMS State Of Charge Value:", BMSSOC() + Pins::getPinValue(PINS_FRONT_PEDAL1)); // Canbus message
+            Log.i(ID, "MC0 Voltage:", MC0Voltage() + testValue);
+            Log.i(ID, "MC1 Voltage:", MC1Voltage() + testValue);
+            Log.i(ID, "Current Power Value:", powerValue() + testValue);   // Canbus message from MCs
+            Log.i(ID, "BMS State Of Charge Value:", BMSSOC() + testValue); // Canbus message
             Log.i(ID, "Fault State", Pins::getCanPinValue(PINS_INTERNAL_GEN_FAULT));
             // if (currentState != &ECUStates::Charging_State || currentState != &ECUStates::Idle_State) {
             //     Pins::setInternalValue(PINS_INTERNAL_CHARGE_SIGNAL, LOW);
