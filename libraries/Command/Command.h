8f1edabca51afd4d80a3ad58c6b7544cd50ca080
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-16 20:11:17 -0500

    Basic layout of command library

---
+++ libraries/Command/Command.h
@@ -0,0 +1,19 @@
+/**
+ * @file Command.h
+ * @author IR
+ * @brief Serial read functionality
+ * @version 0.1
+ * @date 2021-03-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ECU_COMMAND_H__
+#define __ECU_COMMAND_H__
+
+#include "CommandConfig.def"
+#include <stdint.h>
+#include <stdlib.h>
+
+#endif // __ECU_COMMAND_H__
