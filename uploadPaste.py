6cd1ace45b615181a88dce8aaf5826cbbfdfb725
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-03-24 16:58:30 -0500

    switch to paste.ee

--- uploadPaste.py
+++ uploadPaste.py
@@ -1,7 +1,7 @@
 """
 @file uploadPaste.py
 @author IR
-@brief 'Update' pastebin of log_lookup.json for easy download by android app
+@brief 'Update' paste.ee of log_lookup.json for easy download by android app
 @version 0.1
 @date 2021-03-23
 
@@ -13,54 +13,69 @@ from pip._vendor import requests
 import xml.etree.ElementTree as ET
 import json
 from time import sleep
+import asyncio
 
-API = str(base64.b64decode("NEFYcnlGRHBZWjFxUkhBX2FEUS0wTzZtTmRoNldMSjQ==="), "utf-8")
-USER = str(base64.b64decode("MTlhZTQwNDg3OTAyYjljMDRjMjRmYjUyNzZmNmFmZDc="), "utf-8")
-
-
-def deletePaste(paste_key):
-    sleep(1)
-    listParams = {
-        "api_dev_key": API,
-        "api_user_key": USER,
-        "api_paste_key": paste_key,
-        "api_option": "delete",
-    }
-    req = requests.post("https://pastebin.com/api/api_post.php", data=listParams)
-    print(req.status_code, req.reason, req.text)
+DEV = str(base64.b64decode("YTVib2o4bk1NS2w0ZERBazRoT1hrdTRWYkJPNXExUW9HUkJoM0RRSWY="), "UTF-8")
+API = str(base64.b64decode("dVE4NWZCOVVLanRhSnFBazlKVEExaGVVc3J2QURnZVBIejc5RXhKMlo="), "UTF-8")
+headers = {"X-Auth-Token": API}
 
 
-JsonFile = ""
+async def listPastes():
+    IDList = set()
+    status = True
+    req = requests.get("https://api.paste.ee/v1/pastes", headers=headers)
+    if int(req.status_code / 100) == 2:
+        print(req.status_code, req.reason)
+    else:
+        print(req.status_code, req.reason, req.text)
+        return (False, IDList)
+    try:
+        values = json.loads(req.text)
+        for paste in values["data"]:
+            IDList.add(paste["id"])
+    except:
+        status = False
+    return (status, IDList)
 
-try:
-    with open("log_lookup.json", "r", encoding="utf8") as f:
-        JsonFile = f.read()
-except FileNotFoundError:
-    exit("lookup file was not found")
 
-listParams = {
-    "api_dev_key": API,
-    "api_user_key": USER,
-    "api_option": "list",
-}
+async def deletePaste(paste_id):
+    req = requests.delete("https://api.paste.ee/v1/pastes/{}".format(paste_id), headers=headers)
+    print(req.status_code, req.reason, req.text.replace('{"success":true}', "Deleted"))
 
-req = requests.post("https://pastebin.com/api/api_post.php", data=listParams)
-XMLlist = "<pastes>\n" + req.text + "\n</pastes>"
-print(req.status_code, req.reason)
 
-if XMLlist != "No pastes found.":
-    xml = ET.fromstring(XMLlist)
-    for paste in xml.iter("paste_key"):
-        deletePaste(paste.text)
-        print("Delete:", paste.text)
+async def uploadFile():
+    JsonFile = ""
+    try:
+        with open("log_lookup.json", "r", encoding="utf8") as f:
+            JsonFile = f.read()
+    except FileNotFoundError:
+        print("lookup file was not found")
+        return False
 
-params = {
-    "api_paste_name": "JsonMap",
-    "api_paste_private": "0",
-    "api_dev_key": API,
-    "api_user_key": USER,
-    "api_option": "paste",
-    "api_paste_code": JsonFile,
-}
-req = requests.post("https://pastebin.com/api/api_post.php", data=params)
-print(req.status_code, req.reason, req.text)
+    payload = {"sections": [{"name": "JSONMap", "syntax": "json", "contents": JsonFile}]}
+    req = requests.post("https://api.paste.ee/v1/pastes", headers=headers, json=payload)
+    print(req.status_code, req.reason)
+    status = int(req.status_code / 100) == 2
+    idVal = ""
+    if status:  # get ID
+        try:
+            idVal = json.loads(req.text)["id"]
+        except:
+            status = False
+
+    return (status, idVal)  # 2XX code
+
+
+async def main():
+    pasteList = await listPastes()
+    status = await uploadFile()
+
+    if not status[0] or not pasteList[0]:  # unable to upload file
+        exit()
+
+    for pasteID in pasteList[1]:
+        await deletePaste(pasteID)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
