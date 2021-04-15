ab1ee35a80ac028c32886a2134ddd2d3b777f387
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-04-15 00:09:55 -0500

    log graphing

--- interpretPaste.py
+++ interpretPaste.py
@@ -15,6 +15,10 @@ import json
 from time import sleep
 import asyncio
 import sys
+import math
+
+from matplotlib import pyplot as plt
+import numpy as np
 
 GETURL = "https://api.paste.ee/v1/pastes/{}"
 API = str(base64.b64decode("dTBXUXZabUNsdVFkZWJycUlUNjZSRHJoR1paTlVXaXE3U09LTVlPUE8="), "UTF-8")
@@ -51,7 +55,7 @@ def getPaste(id):
 
 
 def usage():
-    print("List available pastes: \t-l\nInterpret paste: \t-p[PasteID]")
+    print("List available pastes: \t-l\nInterpret paste: \t-p[PasteID]\Graph paste: \t-p[PasteID] -g[StringValue]")
 
 
 def interpret(string: str):
@@ -61,11 +65,36 @@ def interpret(string: str):
     StrDict = {v: k for k, v in jsonOBJ[1].items()}
 
     with open("interpret.log", "w") as logFile:
+        logFile.writelines(string[string.find(BeginJSON) + len(BeginJSON) : string.find(EndJSON)])
         for line in dataStream.splitlines():
             msg = line.split(" ")
             if len(msg) != 3:
                 continue
             logFile.write(TagDict[int(msg[0])] + StrDict[int(msg[1])] + " " + str(int(msg[2])) + "\n")
+
+
+def graph(value: str, string: str):
+
+    jsonOBJ = json.loads(string[string.find(BeginJSON) + len(BeginJSON) : string.find(EndJSON)])
+    dataStream = string[string.find(EndJSON) + len(EndJSON) :]
+    strKey = str(jsonOBJ[1][value])
+
+    y = []
+
+    for line in dataStream.splitlines():
+        msg = line.split(" ")
+        if len(msg) != 3 or msg[1] != strKey:
+            continue
+        y.append(int(msg[2]))
+
+    x = np.linspace(0, 1, len(y))
+
+    plt.plot(x, y)
+
+    plt.xlabel("Time Step")
+    plt.ylabel("Value")
+    plt.title(value)
+    plt.savefig("interpret.png")
 
 
 def main():
@@ -76,6 +105,13 @@ def main():
     if sys.argv[1][0:2] == "-l":
         Plist = listPastes()
         exit(Plist[1] if Plist[0] else "Failed to get pastes")
+
+    if len(sys.argv) > 2 and sys.argv[2][0:2] == "-g":
+        id = sys.argv[1][2:]
+        strId = sys.argv[2][2:]
+        print(id, strId)
+        graph(strId, getPaste(id))
+        exit()
 
     if sys.argv[1][0:2] == "-p":
         id = sys.argv[1][2:]
