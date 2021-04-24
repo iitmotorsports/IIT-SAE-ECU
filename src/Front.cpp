d31e67ab3418d6dd777ac7ced956e7d8f0381316
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-04-24 17:00:43 -0500

    even more todos

--- src/Front.cpp
+++ src/Front.cpp
@@ -200,7 +200,8 @@ void Front::run() {
             Log(ID, "BMS Pack Average Current:", BMSAMP());    // Canbus message
             Log(ID, "Fault State", Pins::getCanPinValue(PINS_INTERNAL_GEN_FAULT));
             // TODO: send MC temps, Motor temps, aero angles, DC BUS current
-            // TODO: send batt temp, charge current limit, discharge current limit
+            // TODO: send batt temp, charge current limit, discharge current limit, DC BUS Volt
+            // TODO: update Android with proper string keys
         }
     }
 }
