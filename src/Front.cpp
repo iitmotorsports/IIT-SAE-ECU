7bdf3d85bc7c9c361adc3872bffb8c232a992a9f
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-04 19:53:03 -0500

    comment test loop

--- src/Front.cpp
+++ src/Front.cpp
@@ -237,21 +237,21 @@ void Front::run() {
     //     delay(500);
     // }
 
-    while (true) {
-        Pins::setPinValue(PINS_FRONT_BMS_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_BMS_FAULT));
-        Pins::setPinValue(PINS_FRONT_IMD_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_IMD_FAULT));
-        uint32_t currState = Pins::getCanPinValue(PINS_INTERNAL_STATE);
-        currentState = stateMap[currState]; // returns NULL if not found
-        hasBeat = Heartbeat::checkBeat();
-        static bool on = false;
-        if (hasBeat && (currentState == &ECUStates::Idle_State)) {
-            on = !on;
-        } else {
-            on = 0;
-        }
-        Pins::setPinValue(PINS_FRONT_START_LIGHT, on);
-        delay(200);
-    }
+    // while (true) {
+    //     Pins::setPinValue(PINS_FRONT_BMS_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_BMS_FAULT));
+    //     Pins::setPinValue(PINS_FRONT_IMD_LIGHT, Pins::getCanPinValue(PINS_INTERNAL_IMD_FAULT));
+    //     uint32_t currState = Pins::getCanPinValue(PINS_INTERNAL_STATE);
+    //     currentState = stateMap[currState]; // returns NULL if not found
+    //     hasBeat = Heartbeat::checkBeat();
+    //     static bool on = false;
+    //     if (hasBeat && (currentState == &ECUStates::Idle_State)) {
+    //         on = !on;
+    //     } else {
+    //         on = 0;
+    //     }
+    //     Pins::setPinValue(PINS_FRONT_START_LIGHT, on);
+    //     delay(200);
+    // }
     while (true) {
         if (timeElapsed >= 20) { // High priority updates
             timeElapsed = 0;
