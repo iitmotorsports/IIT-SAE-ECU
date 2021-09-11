e72e69bea39d38a1b8993882b38c50bf913424a1
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-09-11 15:59:10 -0500

    dual external montior task

--- comBoth.bat
+++ comBoth.bat
@@ -1,8 +1,8 @@
 @echo off
-set BACK=COM3
-set FRONT=COM4
-set BAUD=115200
-set MODE=Hex
+set FRONT=%1
+set BACK=%2
+set BAUD=%3
+set MODE=%4
 
 set CALL0=..\TeensyToolchain\tools\ComMonitor.exe %BACK% %BAUD% -t%MODE% -r -w -m8 --jsonPath log_lookup.json
 set CALL1=..\TeensyToolchain\tools\ComMonitor.exe %FRONT% %BAUD% -t%MODE% -r -w -m8 --jsonPath log_lookup.json
