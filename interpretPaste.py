42fb13b1f21893bb77c461d8ff4be6ee973643de
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-14 01:02:02 -0500

    cleanup and use local files as well

--- interpretPaste.py
+++ interpretPaste.py
@@ -11,7 +11,8 @@
 
 List available pastes:  `-l`
 
-This flag pulls the IDs of whatever logs the companion app has uploaded to api.paste.ee (a free text file hosting site).
+This flag pulls the IDs of whatever logs the companion app has
+uploaded to api.paste.ee (a free text file hosting site).
     
 Interpret paste:        `-p[PasteID]`
 
@@ -21,7 +22,8 @@ Where interpreting means to convert the raw numbers of the log file into readabl
 
 Graph paste:            `-p[PasteID] -g`
 
-This argument, when given a valid available ID, will download and interpret the log file into an excel sheet, where each message is graphed.
+This argument, when given a valid available ID, will download and interpret the
+log file into an excel sheet, where each message is graphed.
 
 Graphing uses timestamps that the companion app embeds into each log message.
 
@@ -32,14 +34,11 @@ Graphing uses timestamps that the companion app embeds into each log message.
 # @cond
 
 import base64
-from pip._vendor import requests
 import json
-from time import sleep
-import asyncio
 import sys
-import math
 import os
-import datetime
+
+from pip._vendor import requests
 
 try:
     import openpyxl
@@ -53,156 +52,190 @@ from openpyxl.chart import ScatterChart, Reference, Series
 
 GETURL = "https://api.paste.ee/v1/pastes/{}"
 API = str(base64.b64decode("dTBXUXZabUNsdVFkZWJycUlUNjZSRHJoR1paTlVXaXE3U09LTVlPUE8="), "UTF-8")
-headers = {"X-Auth-Token": API}
-BeginJSON = "---[ LOG MAP START ]---"
-EndJSON = "---[ LOG MAP END ]---"
-SaveName = "_Interpret"
+HEADERS = {"X-Auth-Token": API}
+BEGIN_JSON = "---[ LOG MAP START ]---"
+END_JSON = "---[ LOG MAP END ]---"
+SAVE_NAME = "_Interpret"
 
 
-def listPastes():
-    IDList = set()
+def list_pastes() -> tuple:
+    """List all the pastes on the paste.ee API
+
+    Returns:
+        tuple: (Return Status, set of IDs)
+    """
+    id_list = set()
     status = True
-    req = requests.get("https://api.paste.ee/v1/pastes", headers=headers)
+    req = requests.get("https://api.paste.ee/v1/pastes", headers=HEADERS)
     if int(req.status_code / 100) == 2:
         print(req.status_code, req.reason)
     else:
         print(req.status_code, req.reason, req.text)
-        return (False, IDList)
+        return (False, id_list)
     try:
         values = json.loads(req.text)
         for paste in values["data"]:
-            IDList.add(paste["id"])
+            id_list.add(paste["id"])
     except:
         status = False
-    return (status, IDList)
+    return (status, id_list)
 
 
-def getPaste(id):
-    req = requests.get(GETURL.format(id), headers=headers)
+def get_paste(log_id: str) -> str:
+    """Get a log file from the paste.ee api given it's id string
+
+    Args:
+        log_id (str): id of the log
+
+    Returns:
+        str: The full log file that was uploaded
+    """
+    req = requests.get(GETURL.format(log_id), headers=HEADERS)
     if int(req.status_code / 100) == 2:
         print(req.status_code, req.reason)
     else:
         print(req.status_code, req.reason, req.text)
     raw = json.loads(req.text)["paste"]["sections"][0]["contents"]
-    with open("{}_raw.log".format(SaveName), "w") as rawFile:
-        rawFile.writelines(raw)
+    with open(f"{SAVE_NAME}_raw.log", "w") as raw_file:
+        raw_file.writelines(raw)
 
     return raw
 
 
+def get_log(log_id: str) -> str:
+    """Get a log file given either a paste.ee ID or a local path to one
+
+    Args:
+        log_id (str): id or path of the log
+
+    Returns:
+        str: The full log file
+    """
+    print(f"Parameter Given: {log_id}")
+    if os.path.exists(log_id):
+        with open(log_id, "r") as file:
+            return file.read()
+    return get_paste(log_id)
+
+
 def usage():
+    """Print the usage of this script"""
     print("List available pastes: \t-l\nInterpret paste: \t-p[PasteID]\nGraph paste: \t\t-p[PasteID] -g")
 
 
-def interpret(string: str):
-    jsonOBJ = json.loads(string[string.find(BeginJSON) + len(BeginJSON) : string.find(EndJSON)])
-    dataStream = string[string.find(EndJSON) + len(EndJSON) :]
-    TagDict = {v: k for k, v in jsonOBJ[0].items()}
-    StrDict = {v: k for k, v in jsonOBJ[1].items()}
+def interpret(string: str) -> None:
+    """Interpret a log file and print the interpretation to a local file
 
-    with open("{}.log".format(SaveName), "w") as logFile:
-        logFile.writelines(string[string.find(BeginJSON) + len(BeginJSON) : string.find(EndJSON)])
-        for line in dataStream.splitlines():
+    Args:
+        string (str): The raw log file
+    """
+
+    json_obj = json.loads(string[string.find(BEGIN_JSON) + len(BEGIN_JSON) : string.find(END_JSON)])
+    data_stream = string[string.find(END_JSON) + len(END_JSON) :]
+    tag_dict = {v: k for k, v in json_obj[0].items()}
+    str_dict = {v: k for k, v in json_obj[1].items()}
+
+    with open(f"{SAVE_NAME}.log", "w") as log_file:
+        log_file.writelines(string[string.find(BEGIN_JSON) + len(BEGIN_JSON) : string.find(END_JSON)])
+        for line in data_stream.splitlines():
             msg = line.split(" ")
             if len(msg) != 4:
                 continue
-            # epoch = datetime.datetime.fromtimestamp(int(msg[0]) / 1000.0).strftime("%H:%M:%S:%f")
             epoch = int(msg[0])
-            TagStr = StrStr = "Invalid Msg Key"
+            tag_str = str_str = "Invalid Msg Key"
             try:
-                TagStr = TagDict[int(msg[1])]
+                tag_str = tag_dict[int(msg[1])]
             except KeyError:
                 pass
             try:
-                StrStr = StrDict[int(msg[2])]
+                str_str = str_dict[int(msg[2])]
             except KeyError:
                 pass
-            logFile.write("[{0}] {1: <16s} {2: <35s} {3}\n".format(epoch, TagStr, StrStr, str(int(msg[3]))))
+            log_file.write(f"[{epoch}] {tag_str: <16s} {str_str: <35s} {str(int(msg[3]))}\n")
 
 
-import pprint
+def graph(string: str) -> None:
+    """Interpret and graph a log file to a local excel file
 
-from itertools import repeat
+    Args:
+        string (str): The raw log file
+    """
 
-
-def graph(string: str):
-
-    jsonOBJ = json.loads(string[string.find(BeginJSON) + len(BeginJSON) : string.find(EndJSON)])
-    dataStream = string[string.find(EndJSON) + len(EndJSON) :]
-    StrDict = {v: k for k, v in jsonOBJ[1].items()}
+    json_obj = json.loads(string[string.find(BEGIN_JSON) + len(BEGIN_JSON) : string.find(END_JSON)])
+    data_stream = string[string.find(END_JSON) + len(END_JSON) :]
+    str_dict = {v: k for k, v in json_obj[1].items()}
 
     data = dict()
 
-    dataTitles = list(["Elapsed Time (s)"])
+    data_titles = list(["Elapsed Time (s)"])
 
-    epochSmol = -1
+    epoch_smol = -1
 
-    for line in dataStream.splitlines():
+    for line in data_stream.splitlines():
         try:
             msg = line.split(" ")
-            if len(msg) != 4 or not StrDict.get(int(msg[2])):
+            if len(msg) != 4 or not str_dict.get(int(msg[2])):
                 continue
-            msgID = StrDict[int(msg[2])]
-            if not data.get(msgID):
-                data[msgID] = list()
-                dataTitles.append(msgID)
+            msg_id = str_dict[int(msg[2])]
+            if not data.get(msg_id):
+                data[msg_id] = list()
+                data_titles.append(msg_id)
             epoch = int(msg[0])
-            data[msgID].append(list((epoch, int(msg[3]), data[msgID])))
+            data[msg_id].append(list((epoch, int(msg[3]), data[msg_id])))
 
-            if epochSmol == -1 or epoch < epochSmol:
-                epochSmol = epoch
+            if epoch_smol == -1 or epoch < epoch_smol:
+                epoch_smol = epoch
 
-        except KeyError as e:
-            print("KeyError: ", e)
-            pass
+        except KeyError as err:
+            print("KeyError: ", err)
 
     for _, value in data.items():
         value.sort(key=lambda x: x[0])
         for lst in value:
-            lst[0] -= epochSmol
+            lst[0] -= epoch_smol
             lst[0] /= 1000000000
 
     wb = openpyxl.Workbook()
     ws = wb.active
 
-    ws.append(dataTitles)
+    ws.append(data_titles)
 
     rows = list()
 
-    def getRow():  # IMPROVE: better series generation
+    def get_row():  # IMPROVE: better series generation
         row = list()
-        for title in dataTitles:
+        for title in data_titles:
             column = data.get(title)
             if not column or len(column) == 0:
-                row.append((epochSmol + 1, 0))
+                row.append((epoch_smol + 1, 0))
             else:
                 row.append(column[0])
 
-        smallestEpoch = epochSmol
+        smallest_epoch = epoch_smol
 
         for item in row:
-            if item[0] < smallestEpoch:
-                smallestEpoch = item[0]
+            if item[0] < smallest_epoch:
+                smallest_epoch = item[0]
 
-        if smallestEpoch == epochSmol:
+        if smallest_epoch == epoch_smol:
             return
 
-        finalRow = list([smallestEpoch])
+        final_row = list([smallest_epoch])
         row.pop(0)
 
         for item in row:
-            if item[0] != smallestEpoch:
-                finalRow.append(item[1])
+            if item[0] != smallest_epoch:
+                final_row.append(item[1])
             else:
-                finalRow.append(item[2].pop(0)[1])
+                final_row.append(item[2].pop(0)[1])
 
-        if len(finalRow) == 1:
+        if len(final_row) == 1:
             return
 
-        return finalRow
+        return final_row
 
     while True:
-        row = getRow()
+        row = get_row()
         if row:
             rows.append(row)
         else:
@@ -221,7 +254,7 @@ def graph(string: str):
 
     xvalues = Reference(ws, min_col=1, min_row=2, max_row=len(rows) + 1)
 
-    for i in range(2, len(dataTitles) + 1):
+    for i in range(2, len(data_titles) + 1):
         values = Reference(ws, min_col=i, min_row=1, max_row=len(rows) + 1)
         series = Series(values, xvalues, title_from_data=True)
         # series.marker.symbol = "circle"
@@ -232,29 +265,25 @@ def graph(string: str):
         chart.series.append(series)
 
     ws.add_chart(chart, "A1")
-    wb.save("{}.xlsx".format(SaveName))
+    wb.save("{}.xlsx".format(SAVE_NAME))
 
 
 def main():  # IMPROVE: Better argument handling
-    if len(sys.argv) == 1 or sys.argv[1][0:2] == "-h":
+    """Main Function"""
+    if len(sys.argv) == 1 or sys.argv[1][0:2] == "-h" or not sys.argv[1][2:]:
         usage()
         exit()
 
     if sys.argv[1][0:2] == "-l":
-        Plist = listPastes()
-        exit(Plist[1] if Plist[0] else "Failed to get pastes")
+        p_list = list_pastes()
+        exit(p_list[1] if p_list[0] else "Failed to get pastes")
 
     if len(sys.argv) > 2 and sys.argv[2][0:2] == "-g":
-        id = sys.argv[1][2:]
-        # strId = sys.argv[2][2:]
-        # print(id, strId)
-        graph(getPaste(id))
+        graph(get_log(sys.argv[1][2:]))
         exit()
 
     if sys.argv[1][0:2] == "-p":
-        id = sys.argv[1][2:]
-        print(id)
-        interpret(getPaste(id))
+        interpret(get_log(sys.argv[1][2:]))
         exit()
 
     usage()
