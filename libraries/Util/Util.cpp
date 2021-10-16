0d63f71e1b5c1b586ca1015d2dfbd822d9ba0e4b
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-10-16 00:16:20 -0500

    Squashed commit of the following:
    commit e18afa0d9795d6416d45b045da254c96b44372ab
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Sat Oct 16 00:11:43 2021 -0500
    
        update types
    
    commit 42fb13b1f21893bb77c461d8ff4be6ee973643de
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Thu Oct 14 01:02:02 2021 -0500
    
        cleanup and use local files as well
    
    commit 4c75924e1f0477e0e99928726821f087dfd2ac0c
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Thu Oct 14 00:52:03 2021 -0500
    
        timestamp param
    
    commit 3d419d62618055179dbc3846f8b8e847c51a5b47
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Wed Oct 13 21:49:25 2021 -0500
    
        change logging path
    
    commit 0be87c07742e1532ae8b2c4f8911eb7dbb53aecd
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Wed Oct 13 21:45:23 2021 -0500
    
        add template
    
    commit 253ec0ca5686cc44696aba5997f7af793a9cc6a9
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Wed Oct 13 21:29:28 2021 -0500
    
        enable logging file
    
    commit 735ce1e57ebeef90fcb3c597668424041d6660a8
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Wed Oct 13 21:26:13 2021 -0500
    
        basic value interpolation
    
    commit 5e300a2733c3d204ef977de501276b9e5c621065
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Wed Oct 13 20:45:05 2021 -0500
    
        inital lerping
    
    commit 2fd9fed3435afa9e0f465b548f96df441db589bb
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Fri Oct 1 16:27:30 2021 -0500
    
        print when updating lights
    
    commit 7eec9387439993837be8666d2b438919898739f5
    Author: LeHuman <47549331+LeHuman@users.noreply.github.com>
    Date:   Fri Oct 1 16:27:21 2021 -0500
    
        local ports

---
+++ libraries/Util/Util.cpp
@@ -0,0 +1,35 @@
+#include "Util.h"
+#include "wiring.h"
+
+float interpolate(float t, float i, float f, float d) {
+    // quad
+    t = t / (d * 2);
+    if (t < 1) {
+        return f / 2 * t * t + i;
+    }
+    return -f / 2 * ((t - 1) * (t - 3) - 1) + i;
+
+    // circular
+    // t = t / (d * 2);
+    // if (t < 1) {
+    //     return -f / 2 * (sqrt(1 - t * t) - 1) + i;
+    // }
+    // t = t - 2;
+    // return f / 2 * (sqrt(1 - t * t) + 1) + i;
+
+    // quad rev
+    // if (t < d / 2) {
+    //     return -f / 2 * (pow(t * 2 / d - 1, 4) - 1) + i;
+    // }
+    // return (f / 2) * pow(((t * 2) - d) / d, 4) + (i + f / 2);
+}
+
+// template <class T, class A, class B, class C, class D>
+// T cMap(T x, A inMin, B inMax, C outMin, D outMax) {
+//     T mapped = map(x, inMin, inMax, outMin, outMax);
+//     if (mapped < outMin)
+//         return outMin;
+//     if (mapped > outMax)
+//         return outMax;
+//     return mapped;
+// }
---
+++ libraries/Util/Util.h
@@ -0,0 +1,46 @@
+/**
+ * @file Util.h
+ * @author IR
+ * @brief Various utility functions
+ * @version 0.1
+ * @date 2021-10-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __ECU_UTIL_H__
+#define __ECU_UTIL_H__
+
+/**
+ * @brief Interpolate a value given the parameters
+ * 
+ * @param t 
+ * @param i 
+ * @param f 
+ * @param d 
+ * @return float Interpolated value 
+ */
+float interpolate(float t, float i, float f, float d);
+
+/**
+ * @brief Map a value from one range to another while clamping the value to boundaries
+ * 
+ * @param x The value to be mapped
+ * @param inMin The minimum input value
+ * @param inMax The maximum input value
+ * @param outMin The minimum output value
+ * @param outMax The maximum output value
+ * @return The mapped value for x
+ */
+template <class T, class A, class B, class C, class D>
+T cMap(T x, A inMin, B inMax, C outMin, D outMax) {
+    T mapped = map(x, inMin, inMax, outMin, outMax);
+    if (mapped < outMin)
+        return outMin;
+    if (mapped > outMax)
+        return outMax;
+    return mapped;
+}
+
+#endif // __ECU_UTIL_H__
