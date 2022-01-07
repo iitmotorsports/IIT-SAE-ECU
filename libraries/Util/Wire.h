e4dd2a60490b5addf8d48528c3ddbb7d200ac58f
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-01-07 15:19:01 -0600

    doc wire.h

--- libraries/Util/Wire.h
+++ libraries/Util/Wire.h
@@ -1,3 +1,17 @@
+/**
+ * @file Wire.h
+ * @author IR
+ * @brief Convenience header for alternative libs to the Arduino Wire.h library
+ * @version 0.1
+ * @date 2022-01-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __WIRE_H__
+#define __WIRE_H__
+
 #ifdef __MK66FX1M0__
 #include "i2c_t3.h"
 using TwoWire = i2c_t3;
@@ -6,3 +20,4 @@ using TwoWire = i2c_t3;
 #ifdef __IMXRT1062__
 #include "i2c_driver_wire.h"
 #endif
+#endif // __WIRE_H__
