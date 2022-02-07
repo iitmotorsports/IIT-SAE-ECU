e1604652bd5855c3a3530c7c66f702b132661fa7
LeHuman <LeHuman@users.noreply.github.com>
2022-02-07 00:15:40 +0000

    deploy: 67c4258c432ef25a347aa2e2f72efd971c12b910

--- namespaces_dup.js
+++ namespaces_dup.js
@@ -79,9 +79,11 @@ var namespaces_dup =
       [ "PortPrinter", "classvs__conf_1_1_port_printer.html", null ],
       [ "Settings", "classvs__conf_1_1_settings.html", "classvs__conf_1_1_settings" ],
       [ "comment_remover", "vs__conf_8py.html#a1f1c6513670f3863a1b019fa8eaba30d", null ],
+      [ "get_settings", "vs__conf_8py.html#a439f890015bc5451870eddb56a311f04", null ],
       [ "listify", "vs__conf_8py.html#afc28be17acddac838c2f46460d7fba5c", null ],
       [ "load_json", "vs__conf_8py.html#a9be55bf2bd083f42ee69af1a396dd648", null ],
       [ "main", "vs__conf_8py.html#a59bd943f036ed3f2e69be23ea2bd97de", null ],
-      [ "serial_ports", "vs__conf_8py.html#a75e9e112029aad603942e76f725977b6", null ]
+      [ "serial_ports", "vs__conf_8py.html#a75e9e112029aad603942e76f725977b6", null ],
+      [ "writeBackup", "vs__conf_8py.html#a7c46360ef32fbabe3f52d22c76d28fe0", null ]
     ] ]
 ];
--- search/functions_12.js
+++ search/functions_12.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['w_0',['w',['../struct_logging_1_1_log__t.html#a6a07c7449c92c06a608cb8f6463ebd46',1,'Logging::Log_t::w(LOG_TAG TAG, LOG_MSG message)'],['../struct_logging_1_1_log__t.html#aa09937111a64919316e4119164af89e3',1,'Logging::Log_t::w(LOG_TAG TAG, LOG_MSG message, const uint32_t number, int mediate=false)']]]
+  ['w_0',['w',['../struct_logging_1_1_log__t.html#a6a07c7449c92c06a608cb8f6463ebd46',1,'Logging::Log_t::w(LOG_TAG TAG, LOG_MSG message)'],['../struct_logging_1_1_log__t.html#aa09937111a64919316e4119164af89e3',1,'Logging::Log_t::w(LOG_TAG TAG, LOG_MSG message, const uint32_t number, int mediate=false)']]],
+  ['writebackup_1',['writeBackup',['../vs__conf_8py.html#a7c46360ef32fbabe3f52d22c76d28fe0',1,'vs_conf']]]
 ];
