9a743fbedc02a45f56d47e46af90a95ec8aa84d9
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-04-10 16:34:51 -0500

    call manager

--- libraries/Test/testmod.h
+++ libraries/Test/testmod.h
@@ -5,8 +5,6 @@
 
 void testModules() {
     Log.i("Main", "Starting");
-    threads.setSliceMillis(5);
-    Module::Module_t::startModules();
 
     threads.addThread([](void *) {
         while (1) {
@@ -14,9 +12,5 @@ void testModules() {
             threads.delay(100);
         } }, 0, 2048);
 
-    while (1) {
-        Log.i("Main", "Waiting");
-        threads.delay(10000);
-        Module::Module_t::restartModules();
-    }
+    Module::Module_t::startManager();
 }
