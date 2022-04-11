0dfc8e198c4f557ac05088b957d006963d06cc2f
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-04-10 23:14:18 -0500

    setup to use test

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,18 +1,26 @@
 #include "ECU.h"
-#include "ECUGlobalConfig.h"
-#if CONF_ECU_POSITION == FRONT_ECU
-#include "Front.h"
-#endif
-
-#include "testmod.h"
+#include "threadTest.h"
 
 int main(void) {
     Serial.begin(CONF_ECU_BAUD_RATE);
-    testModules();
-#if CONF_ECU_POSITION == BACK_ECU
-    State::begin(ECUStates::Initialize_State);
-#else
-    Front::run();
-#endif
-    return 0;
+    threadTest();
 }
+
+// #include "ECU.h"
+// #include "ECUGlobalConfig.h"
+// #if CONF_ECU_POSITION == FRONT_ECU
+// #include "Front.h"
+// #endif
+
+// #include "testmod.h"
+
+// int main(void) {
+//     Serial.begin(CONF_ECU_BAUD_RATE);
+//     testModules();
+// #if CONF_ECU_POSITION == BACK_ECU
+//     State::begin(ECUStates::Initialize_State);
+// #else
+//     Front::run();
+// #endif
+//     return 0;
+// }
